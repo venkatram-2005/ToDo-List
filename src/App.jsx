7ecde5ab41b503a3a12a7b8267a6fc4ffa389583
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-02-08 20:27:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDo from "./Components/ToDo"
+
+const App = () => {
+   return (
+    <div>
+        <ToDo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ToDoItems.jsx
@@ -0,0 +1,41 @@
+import './css/ToDoItems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from './Assets/cross.png'
+const ToDoItems = ({no, display, text, settodos}) => {
+
+
+  const del = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos")) ;
+    data = data.filter((todo)=> todo.no !== no) ;
+    settodos(data) ;
+  }
+
+  const toggle = (no) =>{
+    let data = JSON.parse(localStorage.getItem("todos")) ;
+    for(let i=0 ; i<data.length ; i++){
+        if(data[i].no === no){
+            if(data[i].display === ""){
+                data[i].display = "line-through" ;
+            }
+            else{
+                data[i].display = "" ;
+            }
+            break ;
+        }
+    }
+    settodos(data) ;
+  }
+
+  return (
+    <div className="todo-items">
+        <div className={`todo-items-container ${display}`} onClick={()=>{toggle(no)}}>
+            {display==="" ? <img src={not_tick} alt="" /> : <img src={tick} alt="" />}
+            <div className="todo-items-text">{text}</div>
+        </div>
+        <img className='todo-items-cross' onClick={()=>{del(no)}} src={cross} alt="" />
+    </div>
+  )
+}
+
+export default ToDoItems
